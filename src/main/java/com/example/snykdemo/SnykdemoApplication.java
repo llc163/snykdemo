150a4960bd6e618239b9acb8e23a0894a508a60d
Chensi2113 <88916631+Chensi2113@users.noreply.github.com>
2022-06-13 13:14:45 +0800

    init

---
+++ src/main/java/com/example/snykdemo/SnykdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.snykdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnykdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnykdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snykdemo/SnykdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snykdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnykdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
